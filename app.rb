9a0cfd6a01e0c8f976848b1348dad5425fc60b95
Peter <monroepe@gmail.com>
2014-09-08 15:35:46 -0400

    Added page to add new contacts

--- app.rb
+++ app.rb
@@ -3,11 +3,13 @@ require 'sinatra/reloader'
 require "sinatra/activerecord"
 require 'pry'
 
+LIMIT = 4
+
 require_relative 'models/contact'
 
 helpers do
   def on_last_page?(page_num)
-    page_num < (Contact.all.length.to_f / 5).ceil
+    page_num < (Contact.all.length.to_f / LIMIT).ceil
   end
 
   def on_first_page?(page_num)
@@ -22,7 +24,7 @@ get '/' do
     @page_num = 1
   end
 
-  @contacts = Contact.limit(3).offset((@page_num - 1) * 3)
+  @contacts = Contact.limit(LIMIT).offset((@page_num - 1) * 4)
   erb :'contacts/index'
 end
 
@@ -37,6 +39,18 @@ get '/search' do
   erb :'search/index'
 end
 
+get '/new_contact' do
+  erb :'new_contact/index'
+end
+
+post '/new_contact' do
+  first_name = params[:first_name]
+  last_name = params[:last_name]
+  phone_number = params[:phone_number]
+  new_contact = Contact.create(first_name: first_name, last_name: last_name, phone_number: phone_number)
+  new_contact.save
+  redirect '/'
+end
 
 
 
